0c2fac41cc38fc4207d5ad08540fce8e196e218c
Gagah Adilaga <gagahpusokoadilaga03@gmail.com>
2024-09-21 14:28:22 +0700

    membuat file baru agar lebih jelas

---
+++ RotasiReg3-2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+file_name = "Data Stock 20 Sep 2024.xlsx"
+file_path = f"../ROTASI REGION 3/{file_name}"
+master_path = "../ROTASI REGION 3/MASTER REGION STO_NEXT VERSION (62).xlsx"
+df_origin = pd.read_excel(file_path, sheet_name="dos by store-brand type & area")
+df_master = pd.read_excel(master_path, sheet_name="REGION 3", header=1)
+
+df_master_selected = df_master[['SITE CODE', 'PT']]
+df = pd.merge(df_origin, df_master_selected, on='SITE CODE', how='left')
+df[['Sales 30 days', 'DOS 30 days']] = df[['Sales 30 days','DOS 30 days']].map(lambda x: np.nan if x < 0 else x)
+
+
