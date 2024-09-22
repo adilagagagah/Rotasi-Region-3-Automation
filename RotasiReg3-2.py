c0189aad81c226c9a43c3e5861a8f0f274524bee
Gagah Adilaga <gagahpusokoadilaga03@gmail.com>
2024-09-22 15:33:30 +0700

    memperbaiki syarat untuk rotasi

--- RotasiReg3-2.py
+++ RotasiReg3-2.py
@@ -26,42 +26,47 @@ min_dos_df = df[
 min_dos_df = min_dos_df.sort_values(by=['DOS 30 days', 'Sales 30 days'], ascending=[True, False])
 min_dos_code_unique = min_dos_df['SITE CODE'].drop_duplicates().tolist()
 
-code = min_dos_code_unique[0] #E851
-min_dos_code_df = min_dos_df[(min_dos_df['SITE CODE'] == code)]
+for code in min_dos_code_unique:
+    #code = min_dos_code_unique[0] #E851
+    min_dos_code_df = min_dos_df[(min_dos_df['SITE CODE'] == code)]
 
-min_dos_code_store = list(min_dos_code_df['STORE NAME'])[0] + f" ({code})"
-min_dos_code_article = list(min_dos_code_df['Article code no color'])
+    min_dos_code_store = list(min_dos_code_df['STORE NAME'])[0] + f" ({code})"
+    min_dos_code_article = list(min_dos_code_df['Article code no color'])
 
-rotation_df = df[
-    (df['KOTA'] == kota) &
-    (df['DOS 30 days'] >= 45) &
-    (df['Article code no color'].isin(min_dos_code_article))
-]
+    rotation_df = df[
+        (df['PT'] == pt) &
+        (df['KOTA'] == kota) &
+        (df['DOS 30 days'] >= 45) &
+        (df['Article code no color'].isin(min_dos_code_article))
+    ]
 
-for article in min_dos_code_article:
-    min_dos_article_data = min_dos_code_df[min_dos_code_df['Article code no color'] == article]
+    for article in min_dos_code_article:
+        min_dos_article_data = min_dos_code_df[min_dos_code_df['Article code no color'] == article]
 
-    min_stock = min_dos_article_data['Stock'].values[0]
-    min_sales = min_dos_article_data['Sales 30 days'].values[0]
-    min_dos = min_dos_article_data['DOS 30 days'].values[0]
+        min_stock = min_dos_article_data['Stock'].values[0]
+        min_sales = min_dos_article_data['Sales 30 days'].values[0]
+        min_dos = min_dos_article_data['DOS 30 days'].values[0]
 
-    # Data dari rotation_df untuk produk yang cocok
-    rotation_article_data = rotation_df[rotation_df['Article code no color'] == article]
-    rotation_article_data = rotation_article_data.sort_values(by=['DOS 30 days', 'Stock'], ascending=[False, False])
+        # Data dari rotation_df untuk produk yang cocok
+        rotation_article_data = rotation_df[rotation_df['Article code no color'] == article]
+        rotation_article_data = rotation_article_data.sort_values(by=['DOS 30 days', 'Stock'], ascending=[False, False])
 
-    if not rotation_article_data.empty:
-        first_row = rotation_article_data.iloc[0]
+        if not rotation_article_data.empty:
+            first_row = rotation_article_data.iloc[0]
 
-        rotation_store = first_row['STORE NAME'] + f" ({first_row['SITE CODE']})"
-        rotation_stock = first_row['Stock']
-        rotation_sales = first_row['Sales 30 days']
-        rotation_dos = first_row['DOS 30 days']
+            rotation_store = first_row['STORE NAME'] + f" ({first_row['SITE CODE']})"
+            rotation_stock = first_row['Stock']
+            rotation_sales = first_row['Sales 30 days']
+            rotation_dos = first_row['DOS 30 days']
 
-        # Menambahkan ke list hasil
-        result_rows.append([
-            min_dos_code_store, article, min_stock, min_sales, min_dos,
-            rotation_store, rotation_stock, rotation_sales, rotation_dos
-        ])
+            # Menambahkan ke list hasil
+            result_rows.append([
+                min_dos_code_store, article, min_stock, min_sales, min_dos,
+                rotation_store, rotation_stock, rotation_sales, rotation_dos
+            ])
+
+    # result_rows.append([''] * len(result_rows[0]))
+
 
 result_df = pd.DataFrame(result_rows, columns=[
     'STORE ASAL', 'ARTICLE', 'STOCK ASAL', 'SALES ASAL', 'DOS ASAL',
