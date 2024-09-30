7a8a04a7eea7e0a4042d9f8f12bbc05a6a6ed473
Gagah Adilaga <gagahpusokoadilaga03@gmail.com>
2024-09-30 15:25:09 +0700

    melengkapi untuk semua tsh dcm

--- RotasiReg3_DCM.py
+++ RotasiReg3_DCM.py
@@ -11,97 +11,99 @@ df_master_selected = df_master[['SITE CODE', 'PT']]
 df = pd.merge(df_origin, df_master_selected, on='SITE CODE', how='left')
 df = df[["KOTA", "TSH", "SITE CODE", "PT", "STORE NAME","Article code no color", "Stock", "Sales 30 days", "DOS 30 days"]]
 df[['Sales 30 days', 'DOS 30 days']] = df[['Sales 30 days','DOS 30 days']].map(lambda x: np.nan if x < 0 else x)
+dcm_df = []
 
 result_rows = []
 pt = 'DCM'
-df = df[
-    (df['PT'] == pt)
-]
-
+df = df[df['PT'] == pt]
 list_tsh = sorted(df["TSH"].unique())
-tsh = list_tsh[1] #ADE FAJRI
 
-min_dos_df = df[
-    (df['TSH'] == tsh) & 
-    (df['DOS 30 days'] <= 15)
-]
-
-min_dos_df = min_dos_df.sort_values(by=['DOS 30 days', 'Sales 30 days'], ascending=[True, False])
-min_dos_code_unique = min_dos_df['SITE CODE'].unique()
-
-for code in min_dos_code_unique:
-    min_dos_code_df = min_dos_df[(min_dos_df['SITE CODE'] == code)]
-
-    min_dos_code_store = list(min_dos_code_df['STORE NAME'])[0] + f" ({code})"
-    min_dos_code_article = list(min_dos_code_df['Article code no color'])
-
-    rotation_df = df[
+for tsh in list_tsh:
+    min_dos_df = df[
         (df['TSH'] == tsh) & 
-        (df['DOS 30 days'] >= 45) &
-        (df['Article code no color'].isin(min_dos_code_article))
+        (df['DOS 30 days'] <= 15)
     ]
 
-    for article in min_dos_code_article:
-        min_dos_article_data = min_dos_code_df[min_dos_code_df['Article code no color'] == article]
+    min_dos_df = min_dos_df.sort_values(by=['DOS 30 days', 'Sales 30 days'], ascending=[True, False])
+    min_dos_code_unique = min_dos_df['SITE CODE'].unique()
 
-        min_stock, min_sales, min_dos = min_dos_article_data[['Stock', 'Sales 30 days', 'DOS 30 days']].values[0]
+    for code in min_dos_code_unique:
+        min_dos_code_df = min_dos_df[(min_dos_df['SITE CODE'] == code)]
 
-        rotation_df = rotation_df.copy()
-        rotation_df.loc[:, 'Cleaned Article'] = rotation_df['Article code no color'].str.replace('[ /]', '', regex=True)
-        cleaned_article = article.replace(' ', '').replace('/', '')
+        min_dos_code_store = list(min_dos_code_df['STORE NAME'])[0] + f" ({code})"
+        min_dos_code_article = list(min_dos_code_df['Article code no color'])
 
-        # Melakukan pengecekan dengan kolom baru yang 'bersih'
-        rotation_article_data = rotation_df[rotation_df['Cleaned Article'] == cleaned_article]
-        rotation_article_data = rotation_article_data.sort_values(by=['DOS 30 days', 'Stock'], ascending=[False, False])
+        rotation_df = df[
+            (df['TSH'] == tsh) & 
+            (df['DOS 30 days'] >= 45) &
+            (df['Article code no color'].isin(min_dos_code_article))
+        ]
 
-        for _, first_row in rotation_article_data.iterrows():
+        for article in min_dos_code_article:
+            min_dos_article_data = min_dos_code_df[min_dos_code_df['Article code no color'] == article]
 
-            rotation_store = first_row['STORE NAME'] + f" ({first_row['SITE CODE']})"
-            rotation_stock = first_row['Stock']
-            rotation_sales = first_row['Sales 30 days']
-            rotation_dos = first_row['DOS 30 days']
+            min_stock, min_sales, min_dos = min_dos_article_data[['Stock', 'Sales 30 days', 'DOS 30 days']].values[0]
 
-            result_rows.append([
-                tsh, min_dos_code_store, article, min_stock, min_sales, min_dos,
-                rotation_store, rotation_stock, rotation_sales, rotation_dos
-            ])
+            rotation_df = rotation_df.copy()
+            rotation_df.loc[:, 'Cleaned Article'] = rotation_df['Article code no color'].str.replace('[ /]', '', regex=True)
+            cleaned_article = article.replace(' ', '').replace('/', '')
 
-result_df = pd.DataFrame(result_rows, columns=[
-        'TSH', 'STORE TUJUAN', 'ARTICLE', 'STOCK TUJUAN', 'SALES TUJUAN', 'DOS TUJUAN',
-        'STORE ROTASI', 'STOCK ROTASI', 'SALES ROTASI', 'DOS ROTASI'
-    ])
+            # Melakukan pengecekan dengan kolom baru yang 'bersih'
+            rotation_article_data = rotation_df[rotation_df['Cleaned Article'] == cleaned_article]
+            rotation_article_data = rotation_article_data.sort_values(by=['DOS 30 days', 'Stock'], ascending=[False, False])
 
-# Fungsi untuk proses sorting dan pairing untuk setiap artikel
-def process_article(dataframe, article):
-    df_article = dataframe[dataframe['ARTICLE'] == article]
-    sorted_asal = df_article.sort_values(by=['DOS TUJUAN', 'SALES TUJUAN'], ascending=[True, False]).drop_duplicates('STORE TUJUAN')
-    sorted_rotasi = df_article.sort_values(by=['DOS ROTASI', 'STOCK ROTASI'], ascending=[False, False]).drop_duplicates('STORE ROTASI')
-    sorted_asal.reset_index(drop=True, inplace=True)
-    sorted_rotasi.reset_index(drop=True, inplace=True)
-    result = pd.DataFrame({
-        "TSH": sorted_asal['TSH'],
-        "STORE TUJUAN": sorted_asal['STORE TUJUAN'],
-        "ARTICLE": sorted_asal['ARTICLE'],
-        "STOCK TUJUAN": sorted_asal['STOCK TUJUAN'],
-        "SALES TUJUAN": sorted_asal['SALES TUJUAN'],
-        "DOS TUJUAN": sorted_asal['DOS TUJUAN'],
-        "STORE ROTASI": sorted_rotasi['STORE ROTASI'],
-        "STOCK ROTASI": sorted_rotasi['STOCK ROTASI'],
-        "SALES ROTASI": sorted_rotasi['SALES ROTASI'],
-        "DOS ROTASI": sorted_rotasi['DOS ROTASI']
-    })
-    return result
+            for _, first_row in rotation_article_data.iterrows():
 
-articles = result_df['ARTICLE'].unique()
-if not result_df.empty:
-    result_df = pd.concat([process_article(result_df, article) for article in articles])
+                rotation_store = first_row['STORE NAME'] + f" ({first_row['SITE CODE']})"
+                rotation_stock = first_row['Stock']
+                rotation_sales = first_row['Sales 30 days']
+                rotation_dos = first_row['DOS 30 days']
 
-# Menghapus baris dengan nilai NaN (jika ada)
-result_df.dropna(inplace=True)
-result_df.reset_index(drop=True, inplace=True)
+                result_rows.append([
+                    tsh, min_dos_code_store, article, min_stock, min_sales, min_dos,
+                    rotation_store, rotation_stock, rotation_sales, rotation_dos
+                ])
 
+    result_df = pd.DataFrame(result_rows, columns=[
+            'TSH', 'STORE TUJUAN', 'ARTICLE', 'STOCK TUJUAN', 'SALES TUJUAN', 'DOS TUJUAN',
+            'STORE ROTASI', 'STOCK ROTASI', 'SALES ROTASI', 'DOS ROTASI'
+        ])
 
-output_file = "result.xlsx"
+    # Fungsi untuk proses sorting dan pairing untuk setiap artikel
+    def process_article(dataframe, article):
+        df_article = dataframe[dataframe['ARTICLE'] == article]
+        sorted_asal = df_article.sort_values(by=['DOS TUJUAN', 'SALES TUJUAN'], ascending=[True, False]).drop_duplicates('STORE TUJUAN')
+        sorted_rotasi = df_article.sort_values(by=['DOS ROTASI', 'STOCK ROTASI'], ascending=[False, False]).drop_duplicates('STORE ROTASI')
+        sorted_asal.reset_index(drop=True, inplace=True)
+        sorted_rotasi.reset_index(drop=True, inplace=True)
+        result = pd.DataFrame({
+            "TSH": sorted_asal['TSH'],
+            "STORE TUJUAN": sorted_asal['STORE TUJUAN'],
+            "ARTICLE": sorted_asal['ARTICLE'],
+            "STOCK TUJUAN": sorted_asal['STOCK TUJUAN'],
+            "SALES TUJUAN": sorted_asal['SALES TUJUAN'],
+            "DOS TUJUAN": sorted_asal['DOS TUJUAN'],
+            "STORE ROTASI": sorted_rotasi['STORE ROTASI'],
+            "STOCK ROTASI": sorted_rotasi['STOCK ROTASI'],
+            "SALES ROTASI": sorted_rotasi['SALES ROTASI'],
+            "DOS ROTASI": sorted_rotasi['DOS ROTASI']
+        })
+        return result
+
+    articles = result_df['ARTICLE'].unique()
+    if not result_df.empty:
+        result_df = pd.concat([process_article(result_df, article) for article in articles])
+
+    # Menghapus baris dengan nilai NaN (jika ada)
+    result_df.dropna(inplace=True)
+    result_df.reset_index(drop=True, inplace=True)
+
+    dcm_df.append(result_df)
+
+result_df = pd.concat(dcm_df, ignore_index=True)
+result_df = result_df.sort_values(by=['TSH', 'STORE TUJUAN', 'ARTICLE'], ascending=[True, True, True])
+
+output_file = "Rotasi DCM.xlsx"
 result_df.to_excel(output_file, index=False)
 
 print(result_df)
